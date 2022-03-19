683430f796d6b0db1e1061103bd9aa7b6d6b1129
Valeri <veltimofeeva@gmail.com>
2022-03-19 12:50:18 +0400

    added comments and owner checking

--- programs/donation-solana/src/lib.rs
+++ programs/donation-solana/src/lib.rs
@@ -6,24 +6,29 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod donation_solana {
     use super::*;
 
+    //initialize initial program state
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let donation_account = &mut ctx.accounts.donation_account;
         donation_account.owner = *ctx.accounts.user.to_account_info().key;
         Ok(())
     }
 
+    //sent donate
     pub fn make_donation(
         ctx: Context<Donation>,
         lamports: u64,
     ) -> std::result::Result<(), anchor_lang::prelude::ProgramError> {
-        let tx = anchor_lang::solana_program::system_instruction::transfer(
+        let donation_account = &mut ctx.accounts.donation_account;
+        donation_account.donators_list.push(ctx.accounts.user.key());
+
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
             &ctx.accounts.user.key(),
             &ctx.accounts.donation_account.key(),
             lamports,
         );
 
         anchor_lang::solana_program::program::invoke(
-            &tx,
+            &ix,
             &[
                 ctx.accounts.user.to_account_info(),
                 ctx.accounts.donation_account.to_account_info(),
@@ -31,16 +36,21 @@ pub mod donation_solana {
         )
     }
 
+    //withdraw all donates to owner
     pub fn withdraw(
         ctx: Context<Withdraw>,
     ) -> std::result::Result<(), anchor_lang::prelude::ProgramError> {
-        let tx = anchor_lang::solana_program::system_instruction::transfer(
+        if ctx.accounts.user.key() != ctx.accounts.donation_account.owner {
+            return Err(ProgramError::IllegalOwner);
+        }
+
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
             &ctx.accounts.donation_account.key(),
             &ctx.accounts.donation_account.owner,
             ctx.accounts.donation_account.to_account_info().lamports(),
         );
         anchor_lang::solana_program::program::invoke(
-            &tx,
+            &ix,
             &[
                 ctx.accounts.donation_account.to_account_info(),
                 ctx.accounts.user.to_account_info(),
@@ -49,6 +59,7 @@ pub mod donation_solana {
     }
 }
 
+//data for initialize function
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(init, payer = user, space = 64 + 1024)]
@@ -59,17 +70,14 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
+//main program account that hold business logic
 #[account]
 pub struct DonationAccount {
-    pub donators_list: Vec<Donator>,
+    pub donators_list: Vec<Pubkey>,
     pub owner: Pubkey,
 }
 
-#[derive(Clone, Debug, AnchorSerialize, AnchorDeserialize)]
-pub struct Donator {
-    pub user: Pubkey,
-}
-
+//donate function data context
 #[derive(Accounts)]
 pub struct Donation<'info> {
     #[account(mut)]
@@ -77,6 +85,7 @@ pub struct Donation<'info> {
     pub user: Signer<'info>,
 }
 
+//withdraw function data context
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
     #[account(mut)]
